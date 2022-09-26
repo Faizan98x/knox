9718a81f6cb729bc30b1c930571b9db6405d035d
Faizan98x <mohammadfaizan98x@gmail.com>
2022-09-26 21:54:08 +0500

    vol.3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,115 @@
 import React from "react";
 import "../Styles/Footer.css";
-import logo from "../images/logo.png";
+import {
+  FaArrowRight,
+  FaLinkedin,
+  FaPhone,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className="footer">
-      <span>Copyright &#169; 2022</span>
-      <img src={logo} alt="" />
-    </div>
+    <section class="footer" id="footer">
+      <div class="footer-box-container">
+        <div class="footer-box">
+          <h3>quick links</h3>
+          <a href="#">
+            <i>
+              <FaArrowRight />
+            </i>
+            home
+          </a>
+          <a href="#">
+            <i>
+              <FaArrowRight />
+            </i>
+            about
+          </a>
+          <a href="#">
+            <i>
+              <FaArrowRight />
+            </i>
+            services
+          </a>
+          <a href="#">
+            <i>
+              <FaArrowRight />
+            </i>
+            projects
+          </a>
+          <a href="#">
+            <i>
+              <FaArrowRight />
+            </i>
+            contact
+          </a>
+        </div>
+
+        <div class="footer-box">
+          <h3>contact info</h3>
+          <a href="#">
+            <i>
+              <FaPhone />
+            </i>
+            +92-345-1245554
+          </a>
+          <a href="#">
+            <i>
+              <FaPhone />
+            </i>
+            +92-345-1245554
+          </a>
+          <a href="#">
+            <i>
+              <FaEnvelope />
+            </i>
+            muhammadfaizan98x@gmail.com
+          </a>
+          <a href="#">
+            <i>
+              <FaMapMarkerAlt />
+            </i>
+            Islamabad, Pakistan
+          </a>
+        </div>
+
+        <div class="footer-box">
+          <h3>contact info</h3>
+          <a href="#">
+            <i>
+              <FaFacebook />
+            </i>
+            facebook
+          </a>
+          <a href="#">
+            <i>
+              <FaTwitter />
+            </i>
+            twitter
+          </a>
+          <a href="#">
+            <i>
+              <FaInstagram />
+            </i>
+            instagram
+          </a>
+          <a href="#">
+            <i>
+              <FaLinkedin />
+            </i>
+            linkedin
+          </a>
+        </div>
+      </div>
+
+      <div class="credit">
+        all rights reserved <br /> KNOX
+      </div>
+    </section>
   );
 }
 
